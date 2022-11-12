5c830cdde48ec53ba7c54f9c41fd93cca73843b2
fabricio-inoue <103461034+fabricio-inoue@users.noreply.github.com>
2022-11-12 11:13:42 -0300

    initial commit

--- pne.py
+++ pne.py
@@ -7,10 +7,10 @@ import json
 import requests
 import streamlit as st
 import streamlit.components.v1 as components
-from streamlit_option_menu import option_menu
 from streamlit_lottie import st_lottie
+from streamlit_option_menu import option_menu
 
-st.set_page_config(page_title="Projeto Final ")
+st.set_page_config(page_title="Projeto Final", layout="wide")
 
 def load_lottieurl(url):
     r = requests.get(url)
@@ -19,7 +19,9 @@ def load_lottieurl(url):
     return r.json()
 
 #GIFS
+lottie_abertura = load_lottieurl("https://assets6.lottiefiles.com/packages/lf20_4hQRXa.json")
 lottie_database = load_lottieurl("https://assets6.lottiefiles.com/packages/lf20_qp1q7mct.json")
+lottie_exploracao = load_lottieurl("https://assets1.lottiefiles.com/packages/lf20_bdlrkrqv.json")
 lottie_dashboard = load_lottieurl("https://assets6.lottiefiles.com/private_files/lf30_ajzyv37m.json")
 lottie_brainstorm = load_lottieurl("https://assets3.lottiefiles.com/packages/lf20_ayiupfed.json")
 lottie_gamification = load_lottieurl("https://assets9.lottiefiles.com/packages/lf20_ldlns8rs.json")
@@ -32,20 +34,21 @@ lottie_dashboard = load_lottieurl("https://assets6.lottiefiles.com/private_files
 with st.sidebar:
     selected = option_menu(
         menu_title="Menu Principal",
-        options=["Objetivo do projeto", "Plano Nacional de Educação", "Dados Abertos", "Divisão das funções", "Evolução do Projeto"],
+        options=["Objetivo do projeto", "Plano Nacional de Educação", "Dados Abertos", "Divisão das funções", "Evolução do Projeto", "Dashboard", "Análise Estatística"],
         menu_icon="cast",
         default_index=0
     )
 
 # Header
 if selected == "Objetivo do projeto":
-    st.header("Objetivo do projeto")
-    st.subheader("Monitoramento das Metas da PNE")
+    st.title("Objetivo do projeto")
+    st.header("Monitoramento das Metas da PNE")
     st.write("Realizar o acompanhamento das metas do Plano Nacional de Educação (PNE) por meio de análises estatísticas e democratizar o acesso aos seus dados")
+    st_lottie(lottie_abertura, height=300)
     
 if selected == "Plano Nacional de Educação":
-    st.header("O que é a PNE (Plano Nacional de Educação)")
-    st.subheader("O Plano Nacional de Educação (PNE) é uma Lei Federal nº 13.005/2014 com vigência decenal, que foi implementada no governo Dilma. Está de acordo com o artigo 214 da constituição. Suas diretrizes são:")
+    st.title("O que é a PNE (Plano Nacional de Educação)")
+    st.header("O Plano Nacional de Educação (PNE) é uma Lei Federal nº 13.005/2014 com vigência decenal, que foi implementada no governo Dilma. Está de acordo com o artigo 214 da constituição. Suas diretrizes são:")
     st.write("I - erradicação do analfabetismo")
     st.write("II - universalização do atendimento escolar")
     st.write("III - melhoria da qualidade do ensino")
@@ -53,7 +56,7 @@ if selected == "Plano Nacional de Educação":
     st.write("VI - estabelecimento de meta de aplicação de recursos públicos em educação como proporção do produto interno bruto. (Incluído pela Emenda Constitucional nº 59, de 2009)")
 
 if selected == "Dados Abertos":
-    st.subheader("As cinco estrelas dos dados abertos: ⭐ ⭐ ⭐ ⭐ ⭐")
+    st.title("As cinco estrelas dos dados abertos: ⭐ ⭐ ⭐ ⭐ ⭐")
     st.write("Em 2010, o cientista britânico Tim Berners-Lee, inventor da Web, formulou um sistema de estrelas para encorajar a sociedade, especialmente guardiões de dados governamentais, a abrirem seus dados. O sistema ajuda a diagnosticar o nível de abertura de dados dos órgãos públicos e fornece degraus alcançáveis para se chegar a níveis cada vez mais refinados de dados abertos.")
     st.write("⭐ Publicar bases na Web (em qualquer formato) com licença aberta")
     st.write("⭐ ⭐ Publicar bases em formato estruturado com licença aberta (ex: arquivo Excel, em vez de imagem escaneada)")
@@ -63,14 +66,19 @@ if selected == "Dados Abertos":
     st_lottie(lottie_database, height=300)
 
 if selected == "Divisão das funções":
-    st.header("Divisão das funções")
-    st.subheader("Coleta dos Dados")
-    st.write("Coletamos os Dados de bases públicas não estruturadas disponibilizadas pelo governo")
-    st.subheader("Estrutura unificada")
+    st.title("Divisão das funções")
+    st.header("Coleta dos Dados")
+    st.write("Coletamos os Dados de bases públicas não estruturadas disponibilizadas pelo governo. Disponibilidade de dados públicos para o usuário")
+    st_lottie(lottie_exploracao, height=300)
+    st.write("---")
+    st.header("Análise das 20 Metas da PNE")
+    st.write("Análisamos as 20 Metas definidas pela PNE, observamos padrões e definimos pontos focais para o projeto")
+    st.write("---")
+    st.header("Estrutura unificada")
     st.write("Dividimos entre os membros do grupo as 20 metas para serem estruturadas e unificadas em um csv")
-    st.subheader("Construção do Dashboard e Site")
-    st.write("Sumarizamos os Dados coletados em um Dashboard interativo")
-    st.write("[Flourish](https://public.flourish.studio/visualisation/11496831/)")
+    st.write("---")
+    st.header("Construção do Dashboard e Site")
+    st.write("Sumarizamos os Dados coletados em um Dashboard interativo e construímos a interface de um site interativo")
 
 if selected == "Evolução do Projeto":
     st.title("Evolução do Projeto")
@@ -90,12 +98,18 @@ if selected == "Evolução do Projeto":
     st.subheader("Análise do ranking ICE")
     st_lottie(lottie_ice, height=300)
     st.write("O ranking geral do Índice de Cidades Empreendedoras (ICE) 2022 leva em consideração cada um dos sete determinantes - ambiente regulatório, infraestrutura, mercado, acesso a capital, inovação, capital humano e cultura - e seus indicadores")
-    st.write("Problema: Problema: Mostra uma taxa de 0 a 10, mas não apresenta como foram levantadas as variáveis")
+    st.write("Problema: Mostra uma taxa de 0 a 10, mas não apresenta como foram levantadas as variáveis")
     st.write("---")
     st.subheader("Análise das propostas dos candidatos à presidência")
     st_lottie(lottie_politica, height=300)
     st.write("Reunir as propostas dos candidatos, categorizar as propostas e por fim, analisar a performance dos políticos que já possuem carreira política")
     st.write("Problema: Não encontramos dados mensuráveis o suficiente nas propostas")
-    st.write("---")
     
+if selected == "Dashboard":
+    st.title("Dashboard")
+    components.iframe("https://app.powerbi.com/view?r=eyJrIjoiMzlkZThhYjMtNjNhYS00ZDk5LWEzZTQtODhmMGI0MTJkZTk2IiwidCI6IjhlYjI5MjAxLWEyN2QtNDMwMi04NDczLWM5ODJlYjViZTkzNSJ9&pageName=ReportSection3abce68dfea4fa13512a", width=1400, height=850, scrolling=False)
+
+if selected == "Análise Estatística":
+    st.title("Gráfico de Análise Estatística")
+    components.iframe("https://public.flourish.studio/visualisation/11496831/", width=1400, height=850, scrolling=False)
 # %%
